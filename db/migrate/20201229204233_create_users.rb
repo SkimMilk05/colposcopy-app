8119214e6a1efab1f9e3368dca328bc4590c2e54
Sion Kim <sk9uth@virginia.edu>
2020-12-29 15:49:08 -0500

    init

---
+++ db/migrate/20201229204233_create_users.rb
@@ -0,0 +1,16 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.timestamps null: false
+      t.text :email, null: false
+      t.text :first_name, null: false
+      t.text :last_name, null: false
+      t.text :encrypted_password, limit: 128, null: false
+      t.text :confirmation_token, limit: 128
+      t.text :remember_token, limit: 128, null: false
+    end
+
+    add_index :users, :email
+    add_index :users, :remember_token
+  end
+end
---
+++ db/schema.rb
@@ -0,0 +1,31 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_12_29_204233) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.text "email", null: false
+    t.text "first_name", null: false
+    t.text "last_name", null: false
+    t.text "encrypted_password", null: false
+    t.text "confirmation_token"
+    t.text "remember_token", null: false
+    t.index ["email"], name: "index_users_on_email"
+    t.index ["remember_token"], name: "index_users_on_remember_token"
+  end
+
+end
