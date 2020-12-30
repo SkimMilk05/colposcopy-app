3ab91a36476e5420c1b2609f985b8e33fbc29d18
Sion Kim <sk9uth@virginia.edu>
2020-12-30 12:04:16 -0500

    adding .png and .jpeg to asset pipeline

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,6 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
 //= link_directory ../images .JPG
+//= link_directory ../images .png
+//= link_directory ../images .jpeg
 //= link_directory ../audios .mp3
