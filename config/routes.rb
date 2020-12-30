61eef75212ffdd5cebb7fe66f81b3d0305accc26
Sion Kim <sk9uth@virginia.edu>
2020-12-29 21:48:15 -0500

    removing root outside of sign in/out

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   resources :images
   resources :image_sessions
 
-  root to: 'general#index'
+  #root to: 'general#index'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
