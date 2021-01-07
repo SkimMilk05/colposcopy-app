c74acc5e7c7611765f2d0efae95e3af504cd7b78
Sion Kim <sk9uth@virginia.edu>
2021-01-06 23:48:21 -0500

    automatic 5 min timeout

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -2,4 +2,6 @@ Clearance.configure do |config|
   config.routes = false
   config.mailer_sender = "kim.gloria0417@gmail.com"
   config.rotate_csrf_on_sign_in = true
+  #automatic timeout after 5 min
+  config.cookie_expiration = lambda { |cookies| 5.minute.from_now.utc }
 end
