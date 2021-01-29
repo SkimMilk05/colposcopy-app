deb3123b978dd19aaa74adbc16a947f35dcc5715
Sion Kim <sk9uth@virginia.edu>
2021-01-28 19:40:10 -0500

    timeout

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -3,5 +3,5 @@ Clearance.configure do |config|
   config.mailer_sender = "kim.gloria0417@gmail.com"
   config.rotate_csrf_on_sign_in = true
   #automatic timeout after 5 min
-  config.cookie_expiration = lambda { |cookies| 5.minute.from_now.utc }
+  config.cookie_expiration = lambda { |cookies| 1.year.from_now.utc }
 end
