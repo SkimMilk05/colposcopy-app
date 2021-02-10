b8569fd3fb27335da1df465753d9df223d577f25
Sion Kim <sk9uth@virginia.edu>
2021-02-09 22:47:53 -0500

    pass

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -116,7 +116,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'example.com',
+    domain:               'colposcopy-app.herokuapp.com',
     user_name:            ENV['EMAIL_USER'],
     password:             ENV['EMAIL_PASS'],
     authentication:       'plain',
