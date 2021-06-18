fd336bf5360bd3dffe585231b9f0bd7427bd672c
Sion Kim <sk9uth@virginia.edu>
2021-06-17 23:50:13 -0400

    add sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,16 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Add sendgrid support
+if Rails.env.production?
+    ActionMailer::Base.smtp_settings = {
+      :user_name => "apikey",
+      :password => "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",
+      :domain => "colposcopy-app.herokuapp.com",
+      :address => "colproeducationapp@gmail.com",
+      :port => 3000, # sendgrid_port
+      :authentication => :plain,
+      :enable_starttls_auto => true
+    }
+  end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -111,18 +111,18 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
   # Gmail configuration
-  config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'colposcopy-app.herokuapp.com',
-    user_name:            ENV['EMAIL_USER'],
-    password:             ENV['EMAIL_PASS'],
-    authentication:       'plain',
-    enable_starttls_auto: true
-  }
+  # config.action_mailer.smtp_settings = {
+  #   address:              'smtp.gmail.com',
+  #   port:                 587,
+  #   domain:               'colposcopy-app.herokuapp.com',
+  #   user_name:            ENV['EMAIL_USER'],
+  #   password:             ENV['EMAIL_PASS'],
+  #   authentication:       'plain',
+  #   enable_starttls_auto: true
+  # }
 
-  config.action_mailer.default_url_options = { host: 'colposcopy-app.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'colposcopy-app.herokuapp.com', protocol: 'https'  }
 
 end
