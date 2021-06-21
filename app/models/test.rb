315d31b9bcbf17521fda0bd6f5d61d8ca16bc114
Sion Kim <sk9uth@virginia.edu>
2021-06-20 20:43:33 -0400

    Removed test scaffold, ready for hard coding test

--- app/models/test.rb
+++
@@ -1,8 +0,0 @@
-class Test < ApplicationRecord
-   belongs_to :user
-   has_many :test_answers
-   has_many :test_questions, through: :test_answers
-
-   accepts_nested_attributes_for :test_answers
-
-end
--- app/models/test_answer.rb
+++
@@ -1,7 +0,0 @@
-class TestAnswer < ApplicationRecord
-   belongs_to :test
-   belongs_to :test_question
-
-   has_many :test_options, through: :test_questions
-
-end
